dc9adec0ee0d82adfef3122ebec1cc76d0106adf
anna <anna@sayf.su>
2023-01-04 17:49:25 +0300

    task 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+//        task3();
+    }
+
+    /**К нам снова обратились за помощью, но теперь уже для того, чтобы написать алгоритм,
+     * разбивающий одну строку с Ф. И. О. на три — на фамилию, имя и отчество.
+     В качестве исходных данных используйте:
+     Ф. И. О. сотрудника: “Ivanov Ivan Ivanovich”;
+     строка fullName— для хранения Ф. И. О. сотрудника в формате фамилия - имя - отчество;
+     переменная firstName — для хранения имени;
+     переменная middleName — для хранения отчества;
+     переменная lastName — для хранения фамилии.
+     Решите задание с помощью метода substring().
+     Результат программы выведите в формате:
+     “Имя сотрудника — …”
+     “Фамилия сотрудника — …”
+     “Отчество сотрудника — ...”
+
+     *Важно**: «в голове» высчитывать позиции пробелов и параметров для обрезания строки запрещено.
+     Их должен вычислить компьютер.
+
+     - **Подсказка**
+     Определить индексы символов помогут методы indexOf() и lastIndexOf().
+     *
+     */
+    public static void task1() {
+        System.out.println("Задача 1");
+        String fullName = "Ivanov Ivan Ivanovich";
+        String firstName = fullName.substring(fullName.indexOf(" ")+1, fullName.lastIndexOf(" "));
+        System.out.println("Имя сотрудника — " + firstName);
+        String middleName = fullName.substring(fullName.lastIndexOf(" ")+1);
+        System.out.println("Отчество сотрудника — " + middleName);
+        String lastName = fullName.substring(0, fullName.indexOf(" "));
+        System.out.println("Фамилия сотрудника — " + lastName);
+    }
+
+    /**Периодически данные в наших регистрах заполняются неверно,
+     и Ф. И. О. сотрудников записывают со строчных букв.
+     Такую оплошность нужно исправить, написав программу, которая преобразует написанное сострочных букв Ф. И. О.
+     в правильный формат.
+     В качестве исходных данных используйте строку fullName c данными “ivanov ivan ivanovich“,
+     которые нужно преобразовать в “Ivanov Ivan Ivanovich”.
+
+     Выведите результат программы в консоль в формате: “Верное написание Ф. И. О. сотрудника с заглавных букв —  …”
+     - **Подсказка 1**
+     Метод toCharArray() может быть полезен.
+
+     - **Подсказка 2**
+     Для преобразования символа в верхний регистр следует использовать метод Character.toUpperCase(c),
+     где c — символ.
+     */
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "ivanov ivan ivanovich";
+        char[] full = fullName.toCharArray();
+        full[0] = Character.toUpperCase(full[0]);
+        for (int i = 1; i < full.length; i++) {
+            if (full[i] == ' ') {
+                full[i+1] = Character.toUpperCase(full[i+1]);
+            }
+        }
+        String result = new String(full);
+        System.out.println("Верное написание Ф. И. О. сотрудника с заглавных букв — " + result);
     }
 }
